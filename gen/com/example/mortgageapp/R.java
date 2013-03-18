ffa361054c6eb8df83e283c947241c5d7f46d4db
Tri <trishanth@hotmail.com>
2013-03-18 13:23:14 -0700

    finalised app code 03/18/2013

--- gen/com/example/mortgageapp/R.java
+++ gen/com/example/mortgageapp/R.java
@@ -8,37 +8,48 @@
 package com.example.mortgageapp;
 
 public final class R {
+    public static final class array {
+        public static final int years_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int iconretina=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070004;
-        public static final int down_payment=0x7f070001;
-        public static final int menu_settings=0x7f070005;
-        public static final int monthly_payment=0x7f070003;
-        public static final int text_view_label1=0x7f070000;
-        public static final int text_view_label2=0x7f070002;
+        public static final int button1=0x7f080004;
+        public static final int down_payment=0x7f080000;
+        public static final int interest_rate=0x7f080006;
+        public static final int menu_settings=0x7f080009;
+        public static final int monthly_payment=0x7f080001;
+        public static final int text_view_label1=0x7f080003;
+        public static final int text_view_label2=0x7f080002;
+        public static final int text_view_label3=0x7f080005;
+        public static final int text_view_label4=0x7f080007;
+        public static final int years_dropdown=0x7f080008;
     }
     public static final class layout {
         public static final int activity_display_results=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_display_results=0x7f060000;
-        public static final int activity_main=0x7f060001;
+        public static final int activity_display_results=0x7f070000;
+        public static final int activity_main=0x7f070001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int button_submit=0x7f040005;
+        public static final int button_submit=0x7f040008;
         public static final int down_payment=0x7f040002;
-        public static final int hello_world=0x7f040007;
-        public static final int menu_settings=0x7f040006;
+        public static final int hello_world=0x7f04000a;
+        public static final int interest_rate=0x7f040006;
+        public static final int menu_settings=0x7f040009;
         public static final int monthly_payment=0x7f040004;
         public static final int text_view_label1=0x7f040001;
         public static final int text_view_label2=0x7f040003;
-        public static final int title_activity_display_results=0x7f040008;
+        public static final int text_view_label3=0x7f040005;
+        public static final int text_view_label4=0x7f040007;
+        public static final int title_activity_display_results=0x7f04000b;
     }
     public static final class style {
         /** 
@@ -62,10 +73,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/example/mortgageapp/MainActivity.java
+++ src/com/example/mortgageapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.mortgageapp;
 
-//import com.example.myfirstapp.DisplayMessageActivity;
-//import com.example.myfirstapp.R;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -9,9 +7,13 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.AdapterView;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity implements OnItemSelectedListener{
 	
 	public final static String EXTRA_MESSAGE = "com.example.mortgageapp.MESSAGE";
 	
@@ -19,8 +21,28 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Code to populate values in the dropdown list
+		
+	    Spinner spinner = (Spinner) findViewById(R.id.years_dropdown);
+	    
+	    
+	 // Create an ArrayAdapter using the string array and a default spinner layout
+	    
+	 ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+	         R.array.years_array, android.R.layout.simple_spinner_item);
+	 
+	 // Specify the layout to use when the list of choices appears
+	 
+	 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	 
+	 // Apply the adapter to the spinner
+	 
+	 spinner.setAdapter(adapter);
+	 
+	 //spinner.setOnItemSelectedListener(this);
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -28,32 +50,74 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-/**    public void sendMessage(View view) {
-        // Do something in response to button
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.down_payment);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    } */
+	public void onItemSelected(AdapterView<?> parent, View view,int i, long l) {
+        parent.getItemAtPosition(i).toString();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 	
 	public void calMortgage(View view) {
 		
 		Intent intent = new Intent(this, DisplayResultsActivity.class);
 		
-	    EditText text = (EditText) findViewById(R.id.down_payment);
-	    String value = text.getText().toString();       
+	    EditText text1 = (EditText) findViewById(R.id.down_payment);
+	    String value1 = text1.getText().toString();       
 
 	    EditText text2 = (EditText)findViewById(R.id.monthly_payment);
-	    String value2 = text2.getText().toString();              
-          
-	    double l1 = Double.parseDouble(value);
-	    double l2 = Double.parseDouble(value2);
-
-	    double result = l1 + l2;
-	    //String value3 = result.
+	    String value2 = text2.getText().toString();
 	    
-	    String finalresult = Double.toString(result);
+	    EditText text3 = (EditText)findViewById(R.id.interest_rate);
+	    String value3 = text3.getText().toString();
+	    
+	    Spinner spinner = (Spinner) findViewById(R.id.years_dropdown);
+	    spinner.setOnItemSelectedListener(this);
+	    
+	    String value4 = spinner.getSelectedItem().toString();
+	    
+	    System.out.println("Value of value4 is " + value4);
+        
+	    double l1 = Double.parseDouble(value1);
+	    double l2 = Double.parseDouble(value2);
+	    double l3 = Double.parseDouble(value3);
+	    double l4 = Double.parseDouble(value4);
+	    
+	    
+	    System.out.println("Value of l1 is " + l1);
+	    System.out.println("Value of l2 is " + l2);
+	    System.out.println("Value of l3 is " + l3);
+	    System.out.println("Value of l4 is " + l4);
+	    
+	    //double l3 = (double)l3 / 12;
+	    
+	    double r = l3/1200; //to optimize to handle different payment periods.
+	    
+	    double n = l4*12;
+	    
+	    System.out.println("Value of l3 after getting divided by 12 is " + l3);
+
+	    //l3 = l3 / 100;
+	    
+	    System.out.println("Value of l3 after getting divided by 100 is " + l3);
+	    
+	    double t = Math.pow(1+r,n)*r;
+	    
+	   // System.out.println("Value of l5 is " + l5);
+
+	    double b = Math.pow(1+r,n)-1;
+	    
+	   // System.out.println("Value of l6 is " + l6);
+
+	    double result = l2*b/ t;
+	    
+	    //result = ((result*100)+.5)/100;
+	    
+	   double totalpayment = result + l1;
+	    
+	    
+	    
+	    String finalresult = Double.toString(totalpayment);
 	    
 	    intent.putExtra(EXTRA_MESSAGE, finalresult);
 	    
